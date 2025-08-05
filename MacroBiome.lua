8e69b9ab53c72193fcf73fa7c42276e792c01600
OlOlOlBAKA <shanahahaha1@gmail.com>
2025-08-05 20:28:55 +0700

    Update MacroBiome.lua

--- MacroBiome.lua
+++ MacroBiome.lua
@@ -8,6 +8,7 @@ local channel = TextChatService.TextChannels["RBXGeneral"]
 
 local BiomeURL = "https://discord.com/api/webhooks/1401863494181064785/d5jCAV2Uzct4BgQwfhJahYmviX3huH9uzmZUDkF9K7W77YryXQ8bcufDhHlP5DinuhTv"
 local AuraURL = "https://discord.com/api/webhooks/1402263661308936272/QKTw1r0bxbVuZWTivCVlvkrpXwSuYSYfQCWzGpmlos20glq3rVh28TkRcs0TvtU2qfqO"
+local MerchantURL = "https://discord.com/api/webhooks/1402263937071845396/PMHFPLS3OrpYEOR-zp8SHaJ3GB9jW3tjiRLtrUOi0bnUOU-oCLpFSeqVTYMzz2xW_gLu"
 local urlimage = "https://raw.githubusercontent.com/OlOlOlBAKA/Sol-s-RNG/refs/heads/main/images%20(13).jpeg"
 -- credit to regular vynixu
 local Module = loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Utilities/refs/heads/main/Functions.lua"))()
@@ -57,6 +58,8 @@ local keywordsToColor = {
 ["null"] = 0x000000,
 ["manager"] = 0x228B22,
 ["dreamspace"] = 0xFF70D9,
+["mari"] = 0xFFFFFF,
+["jester"] = 0x800080,
 }
 
 local keywordsToDisplayName = {
@@ -71,6 +74,8 @@ local keywordsToDisplayName = {
 ["null"] = "Null",
 ["manager"] = "Glitched",
 ["dreamspace"] = "Dreamspace",
+["mari"] = "Mari",
+["jester"] = "Jester",
 }
 
 local keywordsToImage = {
@@ -85,6 +90,8 @@ local keywordsToImage = {
 ["null"] = "https://raw.githubusercontent.com/OlOlOlBAKA/Sol-s-RNG/refs/heads/main/Null_Biome_(BloxTrap).png",
 ["manager"] = "https://raw.githubusercontent.com/OlOlOlBAKA/Sol-s-RNG/refs/heads/main/Glitch_Tree.png",
 ["dreamspace"] = "https://raw.githubusercontent.com/OlOlOlBAKA/Sol-s-RNG/refs/heads/main/images%20(13).jpeg",
+["mari"] = "https://raw.githubusercontent.com/OlOlOlBAKA/Sol-s-RNG/refs/heads/main/images%20(15).jpeg",
+["jester"] = "https://raw.githubusercontent.com/OlOlOlBAKA/Sol-s-RNG/refs/heads/main/images%20(16).jpeg",
 }
 
 local function findKeyword(text)
@@ -102,6 +109,7 @@ end
 
 TextChatService.OnIncomingMessage = function(message)
     if not message.Text then return end
+    print(message.TextChannel.Name)
     if message.TextChannel and message.TextChannel.Name == "Server Message" then
             local text = message.Text
     text = text:gsub("<.->","")
@@ -137,17 +145,19 @@ print(numberStr)
             end
         end
     end
-    elseif message.TextChannel and message.TextChannel.Name == "General" then
+    elseif message.TextChannel and message.TextChannel.Name == "RBXGeneral" then
           if message.TextSource == nil then
        local keyword, color, display, image = findKeyword(message.Text)
        if keyword then
            local cleanMsg = message.Text:gsub('<font color=".-">', ""):gsub("</font>","")
-           if keyword ~= "merchant" then
+           if keyword ~= "mari" and keyword ~= "jester" then
                SendWebhook("**Biome Detected**", display .. " Has Spawned!", image, color, cleanMsg, BiomeURL)
+           else
+              SendWebhook("**Merchant Detected**", display .. " Has Spawned!", image, color, cleanMsg, MerchantURL)
            end
        end
    end
     end
 end
 
-print("V2 Loaded")
+print("V3 Loaded")
