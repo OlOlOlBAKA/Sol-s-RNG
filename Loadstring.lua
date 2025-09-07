58ab414e0c8b8c45ee0dba9e83cbdbae7898291e
OlOlOlBAKA <shanahahaha1@gmail.com>
2025-09-07 09:23:03 +0700

    Update Loadstring.lua

--- Loadstring.lua
+++ Loadstring.lua
@@ -1,1 +1,20 @@
-_G.WindyPing = ""
+-- Webhooks Type Your Webhook Link here
+_G.BiomeWebhook
+_G.AuraWebhook
+_G.MerchantWebhook
+-- Biome Ping Type Your Role ID here
+_G.SandStorm
+_G.Hell
+_G.Starfall
+_G.Corruption
+_G.Null
+-- For Glitched And Dreamspace You Need To Type Only true or false for allow ping everyone or not
+_G.Glitched
+_G.Dreamspace
+-- Merchant Ping Type Your Role ID here
+_G.Jester
+-- Auras Ping Type Your Role ID here
+_G.Globals
+_G.OneBillion
+-- Eden (ONLY FOR PERSON WHO USED MACRO) Role ID too
+_G.Eden
