28c5a19b2fec82daf6917bad4d33764f971c326a
OlOlOlBAKA <shanahahaha1@gmail.com>
2025-10-23 19:11:07 +0700

    Create LoadstringMusic.lua

---
+++ LoadstringMusic.lua
@@ -0,0 +1,8 @@
+-- By Disable You need to clear script then execute code "_G.MusicEnabled = false"
+_G.MusicEnabled = true
+-- Link Of Music
+_G.MusicLink = ""
+-- Music File Name if you want to use another music do not use same name with the previous one but if you want to execute the previous one just change file name to the previous one
+_G.MusicFileName = ""
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/OlOlOlBAKA/Sol-s-RNG/refs/heads/main/CustomMusic.lua"))()
