67e7df8617ce9804cafc1de0ed1a3025c06726c6
OlOlOlBAKA <shanahahaha1@gmail.com>
2026-02-15 22:33:44 +0700

    Update LastestTest.lua

--- LastestTest.lua
+++ LastestTest.lua
@@ -1,5 +1,3 @@
-if game.PlaceId ~= "15532962292" then return end
-
 local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 if not _G["AlreadyRun"] then
     _G["AlreadyRun"] = true
