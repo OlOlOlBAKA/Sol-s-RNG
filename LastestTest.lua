a966f04eb0eeac52b247bd56c0ef323fb2266024
OlOlOlBAKA <shanahahaha1@gmail.com>
2026-02-21 20:17:44 +0700

    Update currentVersion to 3.2.4 and refactor saving

--- LastestTest.lua
+++ LastestTest.lua
@@ -76,7 +76,7 @@ _G.OneBillion = ""
 _G.Native = ""
 _G.Eden = ""
 
-local currentVersion = "3.2.3"
+local currentVersion = "3.2.4"
 local macroLOGO = "https://images-ext-1.discordapp.net/external/5SQy_HFQ9qnNKrpddi_zNH1Nb9t10WXPqggSJqVg_A8/%3Fcb%3D20260101162000/https/cdn.mongoosee.com/assets/biomes/GLITCHED.png"
 
 local HttpService = game:GetService("HttpService")
@@ -329,33 +329,33 @@ local function SendStatusWebhook()
                     ["icon_url"] = macroLOGO,
                 },
                 ["fields"] = {
-    {["name"] = "Total Normal Biomes Found", ["value"] = _G.totalWindy + _G.totalSnowy + _G.totalRainy, ["inline"] = false},
-    {["name"] = "Windy",                     ["value"] = _G.totalWindy,   ["inline"] = false},
-    {["name"] = "Snowy",                     ["value"] = _G.totalSnowy,   ["inline"] = false},
-    {["name"] = "Rainy",                     ["value"] = _G.totalRainy,   ["inline"] = false},
+                    {["name"] = "Total Normal Biomes Found", ["value"] = _G.totalWindy + _G.totalSnowy + _G.totalRainy, ["inline"] = false},
+                    {["name"] = "Windy",                     ["value"] = _G.totalWindy,   ["inline"] = false},
+                    {["name"] = "Snowy",                     ["value"] = _G.totalSnowy,   ["inline"] = false},
+                    {["name"] = "Rainy",                     ["value"] = _G.totalRainy,   ["inline"] = false},
 
-    {["name"] = "Total Rare Biomes Found",   ["value"] = _G.totalSandStorm + _G.totalHell + _G.totalStarfall + _G.totalHeaven + _G.totalCorruption + _G.totalNull + _G.totalGlitched + _G.totalDreamspace + _G.totalCyberspace, ["inline"] = false},
-    {["name"] = "Sand Storm",                ["value"] = _G.totalSandStorm,   ["inline"] = false},
-    {["name"] = "Hell",                      ["value"] = _G.totalHell,        ["inline"] = false},
-    {["name"] = "Starfall",                  ["value"] = _G.totalStarfall,    ["inline"] = false},
-    {["name"] = "Heaven",                    ["value"] = _G.totalHeaven,      ["inline"] = false},
-    {["name"] = "Corruption",                ["value"] = _G.totalCorruption,  ["inline"] = false},
-    {["name"] = "Null",                      ["value"] = _G.totalNull,        ["inline"] = false},
-    {["name"] = "Glitched",                  ["value"] = _G.totalGlitched,    ["inline"] = false},
-    {["name"] = "Dreamspace",                ["value"] = _G.totalDreamspace,  ["inline"] = false},
-    {["name"] = "Cyberspace",                ["value"] = _G.totalCyberspace,  ["inline"] = false},
+                    {["name"] = "Total Rare Biomes Found",   ["value"] = _G.totalSandStorm + _G.totalHell + _G.totalStarfall + _G.totalHeaven + _G.totalCorruption + _G.totalNull + _G.totalGlitched + _G.totalDreamspace + _G.totalCyberspace, ["inline"] = false},
+                    {["name"] = "Sand Storm",                ["value"] = _G.totalSandStorm,   ["inline"] = false},
+                    {["name"] = "Hell",                      ["value"] = _G.totalHell,        ["inline"] = false},
+                    {["name"] = "Starfall",                  ["value"] = _G.totalStarfall,    ["inline"] = false},
+                    {["name"] = "Heaven",                    ["value"] = _G.totalHeaven,      ["inline"] = false},
+                    {["name"] = "Corruption",                ["value"] = _G.totalCorruption,  ["inline"] = false},
+                    {["name"] = "Null",                      ["value"] = _G.totalNull,        ["inline"] = false},
+                    {["name"] = "Glitched",                  ["value"] = _G.totalGlitched,    ["inline"] = false},
+                    {["name"] = "Dreamspace",                ["value"] = _G.totalDreamspace,  ["inline"] = false},
+                    {["name"] = "Cyberspace",                ["value"] = _G.totalCyberspace,  ["inline"] = false},
 
-    {["name"] = "Total Auras Found (1M+ only)", ["value"] = _G.total1M + _G.total10M + _G.total100M + _G.total1B, ["inline"] = false},
-    {["name"] = "1M Auras",                  ["value"] = _G.total1M,     ["inline"] = false},
-    {["name"] = "10M Auras",                 ["value"] = _G.total10M,    ["inline"] = false},
-    {["name"] = "100M Auras",                ["value"] = _G.total100M,   ["inline"] = false},
-    {["name"] = "1B Auras",                  ["value"] = _G.total1B,     ["inline"] = false},
+                    {["name"] = "Total Auras Found (1M+ only)", ["value"] = _G.total1M + _G.total10M + _G.total100M + _G.total1B, ["inline"] = false},
+                    {["name"] = "1M Auras",                  ["value"] = _G.total1M,     ["inline"] = false},
+                    {["name"] = "10M Auras",                 ["value"] = _G.total10M,    ["inline"] = false},
+                    {["name"] = "100M Auras",                ["value"] = _G.total100M,   ["inline"] = false},
+                    {["name"] = "1B Auras",                  ["value"] = _G.total1B,     ["inline"] = false},
 
-    {["name"] = "Total Merchants Found",     ["value"] = _G.totalMari + _G.totalRin + _G.totalJester, ["inline"] = false},
-    {["name"] = "Mari",                      ["value"] = _G.totalMari,   ["inline"] = false},
-    {["name"] = "Rin",                       ["value"] = _G.totalRin,    ["inline"] = false},
-    {["name"] = "Jester",                    ["value"] = _G.totalJester, ["inline"] = false},
-}
+                    {["name"] = "Total Merchants Found",     ["value"] = _G.totalMari + _G.totalRin + _G.totalJester, ["inline"] = false},
+                    {["name"] = "Mari",                      ["value"] = _G.totalMari,   ["inline"] = false},
+                    {["name"] = "Rin",                       ["value"] = _G.totalRin,    ["inline"] = false},
+                    {["name"] = "Jester",                    ["value"] = _G.totalJester, ["inline"] = false},
+                }
             }}
         })
     })
@@ -441,32 +441,38 @@ end
 
 local function saveBiomes()
     local data = {
-        "totalWindy = " .. totalWindy,
-        "totalSnowy = " .. totalSnowy,
-        "totalRainy = " .. totalRainy,
-        "totalSandStorm = " .. totalSandStorm,
-        "totalHell = " .. totalHell,
-        "totalStarfall = " .. totalStarfall,
-        "totalHeaven = " .. totalHeaven,
-        "totalCorruption = " .. totalCorruption,
-        "totalNull = " .. totalNull,
-        "totalGlitched = " .. totalGlitched,
-        "totalDreamspace = " .. totalDreamspace,
-        "totalCyberspace = " .. totalCyberspace,
-        "total1M = " .. total1M,
-        "total10M = " .. total10M,
-        "total100M = " .. total100M,
-        "total1B = " .. total1B,
-        "totalMari = " .. totalMari,
-        "totalRin = " .. totalRin,
-        "totalJester = " .. totalJester
+        "totalWindy = " .. _G.totalWindy,
+        "totalSnowy = " .. _G.totalSnowy,
+        "totalRainy = " .. _G.totalRainy,
+        "totalSandStorm = " .. _G.totalSandStorm,
+        "totalHell = " .. _G.totalHell,
+        "totalStarfall = " .. _G.totalStarfall,
+        "totalHeaven = " .. _G.totalHeaven,
+        "totalCorruption = " .. _G.totalCorruption,
+        "totalNull = " .. _G.totalNull,
+        "totalGlitched = " .. _G.totalGlitched,
+        "totalDreamspace = " .. _G.totalDreamspace,
+        "totalCyberspace = " .. _G.totalCyberspace,
+        "total1M = " .. _G.total1M,
+        "total10M = " .. _G.total10M,
+        "total100M = " .. _G.total100M,
+        "total1B = " .. _G.total1B,
+        "totalMari = " .. _G.totalMari,
+        "totalRin = " .. _G.totalRin,
+        "totalJester = " .. _G.totalJester
     }
     
     local content = table.concat(data, "\n")
     
-    -- replaced file
-    writefile(saveFile, content)
-    print("Successfully saved file " .. saveFile)
+    local success, err = pcall(function()
+        writefile(saveFile, content)
+    end)
+    
+    if success then
+        print("Successfully saved file: " .. saveFile)
+    else
+        warn("Failed to save file " .. saveFile .. ": " .. tostring(err))
+    end
 end
 
 local function loadBiomes()
